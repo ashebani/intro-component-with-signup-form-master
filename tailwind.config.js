e02b2ec642f8f25be9d9e0f449e05c10fbe0d070
ashebani <azizoo2001.as@gmail.com>
2023-05-06 15:05:16 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      Red: "hsl(0, 100%, 74%)",
+      Green: "hsl(154, 59%, 51%)",
+      Blue: "hsl(248, 32%, 49%)",
+      neutralDarkBlue: "hsl(249, 10%, 26%)",
+      neutralGrayishBlue: "hsl(246, 25%, 77%)",
+      neutralWhite: "#fff",
+    },
+  },
+  plugins: [],
+};
